7d40deb7a0f6b328173bd32160a38629b5b4ca61
hirohirooo <126838535+hirohirooo@users.noreply.github.com>
2023-05-19 21:26:46 +0900

    edit user.edit.blade.php

--- laravel/resources/views/user/index.blade.php
+++ laravel/resources/views/user/index.blade.php
@@ -3,49 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <title>座席表</title>
-    <link rel="stylesheet" href="{{ asset('asset/css/index.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 
-<header style="background-color: #ff7f50;">
-    <a href="{{ route('login') }}" class="btn btn-danger">管理者用ログインページはこちら</a>
-    <h1 class="text-center">座席表</h1>
-    <div class="clock"></div>
-</header>
-{{--<div class="alert alert-info">--}}
-{{--    <strong><p class=" text-center">空いている座席を選択してください</p></strong>--}}
-{{--</div>--}}
-<main style="background-color: #fff3e6">
-        <div class="seats">
-            @for($i = 1; $i <= 40; $i++)
-                @if(!is_null($users->first()) && $users->where('id', $i)->whereNotNull('updated_at')->count() > 0)
-                    <div class="seat reserved">
-                            <?php
-                            $time = $users->where('id', $i)->whereNotNull('updated_at')->first()->time;
-                            ?>
-                        <span>~{{ $time }}時</span><br>
-                    </div>
-                @else
-                    <a href="{{ route('user.edit.{seat_id}', ['seat_id' => $i]) }}">
-                        <div class="seat">
-                            {{ $i }}<br>
-                        </div>
-                    </a>
-                @endif
-            @endfor
-
-
-
+<div class="container-fluid">
+    <div class="row">
+    <header style="background-color: #ff7f50;">
+        <a href="{{ route('login') }}" class="btn btn-secondary btn-sm">管理者用ログインページ</a>
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center">座席表</h1>
+            </div>
+            <div class="col">
+                <div class="clock text-center fs-2"></div>
+            </div>
         </div>
-        <script src="{{ asset('asset/js/index.js') }}"></script>
-</main>
+    </header>
+    </div>
+</div>
+<div class="container-fluid"  style="background-color: #fff3e6">
+    <main>
+        <div class="row text-center">
+                    @for($i = 1; $i <= 40; $i++)
+                        @if(!is_null($users->first()) && $users->where('id', $i)->whereNotNull('updated_at')->count() > 0)
+                                        <?php
+                                        $time = $users->where('id', $i)->whereNotNull('updated_at')->first()->time;
+                                        ?>
+                                <a class="col-3 btn btn-outline-dark disabled" aria-disabled="true">
+                                    ~{{ $time }}時<br>
+                                </a>
+                        @else
+                                <a class="col-3 btn btn-outline-warning" href="{{ route('user.edit.{seat_id}', ['seat_id' => $i]) }}">
+                                    {{ $i }}<br>
+                                </a>
 
+                        @endif
+                    @endfor
+                </div>
+            <script src="{{ asset('asset/js/index.js') }}"></script>
+    </main>
+</div>
 </body>
-
-
-
 </html>
 
 
